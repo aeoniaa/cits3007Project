aa58baeed6062057c62be77bb8898e21baba8444
aeoniaa <100823361+aeoniaa@users.noreply.github.com>
2023-10-07 22:39:59 +0800

    Update p_and_p.c

--- p_and_p.c
+++ p_and_p.c
@@ -1,4 +1,7 @@
 //This is a changed file
+//TODO: check when reading a buffer no use of strcpy or strlen as they read past the buffer size DEFAULT_BUFFER_SIZE-1
+//could use <read> func from <unistd.h> to read struct from file using fd ssize_tread(intfd, void*buf, size_tcount); -->   structMyStructmyStruct; ssize_tres = read(fd, &myStruct, sizeof(structMyStruct));
+//fp to fd --> fdopen and fileno
 
 #include <p_and_p.h>
 
@@ -16,7 +19,7 @@
  * \return 1 if error, 0 otherwise
 */
 int saveItemDetails(const struct ItemDetails* arr, size_t numEls, int fd){
-    //TODO: DOES THE FD NEED TO BE ZEROED OUT FIRST
+    //TODO: ItemDetails Struct --> uint64_t is 8, the buffers are 512,  total 1032bytes
     FILE *fp;
 
     //open file
@@ -41,6 +44,7 @@ int saveItemDetails(const struct ItemDetails* arr, size_t numEls, int fd){
       return 1;
     }
 
+    //TODO: memset() STRUCT NEED TO BE ZEROED OUT FIRST -> if itemdetails doesnt have exact known things (ie, theres no pointers)
     //write the structs
     size_t els_written = fwrite(&arr, sizeof(struct ItemDetails), numEls, fp);
     if (els_written != numEls) {
@@ -48,9 +52,7 @@ int saveItemDetails(const struct ItemDetails* arr, size_t numEls, int fd){
     return 1;
     }
 
-    //fflush()
-    
-
+    //TODO: fflush() --> check if contains buffered inout or output yet to be fully read or written
     fclose(fp);
     return 0;
   }
@@ -71,6 +73,12 @@ int saveItemDetailsToPath(const struct ItemDetails* arr, size_t numEls, const ch
 int loadItemDetails(struct ItemDetails** ptr, size_t* numEls, int fd) {
   return 0;
   //reads data from the .dat files
+    //fread to read info from a struct
+
+    // file open
+    //do work
+    // //TODO: fflush()
+    //fclose(fp);
 }
 
 int isValidName(const char *str) {
@@ -90,16 +98,33 @@ int isValidCharacter(const struct Character * c) {
 }
 
 int saveCharacters(struct Character *arr, size_t numEls, int fd) {
-  return 0;
+    //Full marks only needs functions that work with known valid files
+    // Character struct --> the inventorySize says how many records are in the items carried array
+// When reading from file you need to know how much memory you are reading, its variable depending on inventory size
+  
+    //TODO: fwrite characters struct
+      //TODO: STRUCT NEED TO BE ZEROED OUT FIRST then add in the name. Find a function that takes a value and copies it through the whole array
+     
+    
+    // file open
+    //do work
+    // //TODO: fflush()
+    //fclose(fp);
+    return 0;
 }
 
 int loadCharacters(struct Character** ptr, size_t* numEls, int fd) {
-  return 0;
+      // file open
+    //do work
+    // //TODO: fflush()
+    //fclose(fp);
+    return 0;
 }
 
 int secureLoad(const char *filepath) {
   return 0;
   //find where to obtain ItemDetails pte and numEls variable in order to pass into playGame()
+    //setuid: secure coding cookbook, labs
   //calls playGame()
 }
 
