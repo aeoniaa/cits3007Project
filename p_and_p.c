ba9fb67853cbb8a51281c399e59f7a71560c062c
aeoniaa <100823361+aeoniaa@users.noreply.github.com>
2023-10-09 18:39:45 +0800

    Update p_and_p.c

--- p_and_p.c
+++ p_and_p.c
@@ -54,11 +54,9 @@ int saveItemDetails(const struct ItemDetails* arr, size_t nmemb, int fd) {
   //--> add struct afterwards
 
   //write nmemb to file
-  //TODO: what is nmemb???? numItems is its original name
   size_t numStructs = nmemb;
   printf("nmemb: %ld\n", nmemb);
   
-  //TODO: remove before submitting
   for (size_t i = 0; i < nmemb; i++) {
     printf("Item %zu:\n", i + 1);
     printf("\tItem ID: %lu\n", arr[i].itemID);
@@ -84,8 +82,7 @@ int saveItemDetails(const struct ItemDetails* arr, size_t nmemb, int fd) {
 
   //write the structs, returns num of elements written
   //TODO: IS THIS NECCESSARY: malloc memory size of file? memset(to NULL?), write in structs?
-  size_t els_written = fwrite(&arr, DEFAULT_BUFFER_SIZE+DEFAULT_BUFFER_SIZE+sizeof(uint64_t), nmemb, fp);
-  //size_t els_written = fwrite(&arr, sizeof(struct ItemDetails), nmemb, fp);
+  size_t els_written = fwrite(&arr, sizeof(struct ItemDetails), nmemb, fp);
   printf("els_written: %ld", els_written);
   if (els_written != nmemb) {
     fclose(fp);
